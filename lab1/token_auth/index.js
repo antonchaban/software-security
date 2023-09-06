3cfbbf58bbe7f1906b3c9f771a3fa93c8520421b
Anton Chaban <a.chaban-2027@kpi.ua>
2023-09-06 20:44:42 +0300

    implemented auth with jwt token

--- lab1/token_auth/index.js
+++ lab1/token_auth/index.js
@@ -1,101 +1,49 @@
-const uuid = require('uuid');
 const express = require('express');
-const onFinished = require('on-finished');
 const bodyParser = require('body-parser');
 const path = require('path');
+const jwt = require('jsonwebtoken');
 const port = 3000;
-const fs = require('fs');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 const SESSION_KEY = 'Authorization';
 
-class Session {
-    #sessions = {}
+function verifyToken(token) {
+    if (!token) return null;
 
-    constructor() {
-        try {
-            this.#sessions = fs.readFileSync('./sessions.json', 'utf8');
-            this.#sessions = JSON.parse(this.#sessions.trim());
+    const data = jwt.verify(token, process.env.JWT_SECRET);
+    if (!data) return null;
 
-            console.log(this.#sessions);
-        } catch(e) {
-            this.#sessions = {};
-        }
-    }
-
-    #storeSessions() {
-        fs.writeFileSync('./sessions.json', JSON.stringify(this.#sessions), 'utf-8');
-    }
-
-    set(key, value) {
-        if (!value) {
-            value = {};
-        }
-        this.#sessions[key] = value;
-        this.#storeSessions();
-    }
-
-    get(key) {
-        return this.#sessions[key];
-    }
-
-    init(res) {
-        const sessionId = uuid.v4();
-        this.set(sessionId);
-
-        return sessionId;
-    }
-
-    destroy(req, res) {
-        const sessionId = req.sessionId;
-        delete this.#sessions[sessionId];
-        this.#storeSessions();
-    }
+    const user = users.find(user => user.login == data.login);
+    if (!user) return null;
+    return user.username;
 }
 
-const sessions = new Session();
-
 app.use((req, res, next) => {
-    let currentSession = {};
-    let sessionId = req.get(SESSION_KEY);
+    const sessionId = req.get(SESSION_KEY);
 
-    if (sessionId) {
-        currentSession = sessions.get(sessionId);
-        if (!currentSession) {
-            currentSession = {};
-            sessionId = sessions.init(res);
-        }
-    } else {
-        sessionId = sessions.init(res);
-    }
-
-    req.session = currentSession;
+    req.username = verifyToken(sessionId);
     req.sessionId = sessionId;
-
-    onFinished(req, () => {
-        const currentSession = req.session;
-        const sessionId = req.sessionId;
-        sessions.set(sessionId, currentSession);
-    });
 
     next();
 });
 
 app.get('/', (req, res) => {
-    if (req.session.username) {
+    if (req.username) {
         return res.json({
-            username: req.session.username,
-            logout: 'http://localhost:3000/logout'
-        })
+            username: req.username,
+            logout: 'http://localhost:3000/logout',
+        });
     }
-    res.sendFile(path.join(__dirname+'/index.html'));
-})
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.get('/logout', (req, res) => {
-    sessions.destroy(req, res);
     res.redirect('/');
 });
 
@@ -110,28 +58,27 @@ const users = [
         password: 'Password1',
         username: 'Username1',
     }
-]
+];
 
 app.post('/api/login', (req, res) => {
-    const { login, password } = req.body;
+    const {login, password} = req.body;
 
-    const user = users.find((user) => {
+    const user = users.find(user => {
         if (user.login == login && user.password == password) {
             return true;
         }
-        return false
+        return false;
     });
 
     if (user) {
-        req.session.username = user.username;
-        req.session.login = user.login;
-
-        res.json({ token: req.sessionId });
+        const token = jwt.sign({login: user.login}, process.env.JWT_SECRET);
+        console.log(token);
+        res.json({token});
     }
 
     res.status(401).send();
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
