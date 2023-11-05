986739c3596f8678b3afaca3782c7dd84e01c640
Anton Chaban <a.chaban.fict@gmail.com>
2023-11-05 13:35:19 +0200

    lab 5 + 6

---
+++ lab5/scripts/index.js
@@ -0,0 +1,49 @@
+const {
+    addTokenFromCookieToHeadersMiddleware,
+    checkJwtMiddleware,
+    unauthorizedMiddleware,
+} = require('./middleware');
+const { login, register, logout, getServerToken } = require('./service');
+const path = require('path');
+const request = require('request');
+require('dotenv-expand').expand(require('dotenv').config());
+
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//public endpoints
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages/index.html'));
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages/register.html'));
+});
+app.post('/api/login', (req, res) => login(req, res));
+app.post('/api/register', (req, res) => register(req, res));
+
+//middleware
+app.use(addTokenFromCookieToHeadersMiddleware);
+app.use(checkJwtMiddleware);
+app.use(unauthorizedMiddleware);
+
+//protected endpoints
+app.get('/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, '../pages/dashboard.html'));
+});
+app.post('/api/logout', (req, res) => logout(req, res));
+
+getServerToken();
+setInterval(() => getServerToken(), 82800000); //23h
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+    console.log('process.env.DOMAIN:', process.env.DOMAIN);
+});
---
+++ lab5/scripts/service.js
@@ -0,0 +1,110 @@
+require('dotenv-expand').expand(require('dotenv').config());
+const request = require('request');
+
+const login = (req, res) => {
+    const { login, password } = req.body;
+
+    request(
+        {
+            method: 'POST',
+            url: `https://${process.env.DOMAIN}/oauth/token`,
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            form: {
+                audience: process.env.AUDIENCE,
+                grant_type: 'http://auth0.com/oauth/grant-type/password-realm',
+                client_id: process.env.CLIENT_ID,
+                client_secret: process.env.CLIENT_SECRET,
+                username: login,
+                password: password,
+                scope: 'offline_access',
+                realm: 'Username-Password-Authentication',
+            },
+        },
+        (error, response, body) => {
+            if (error) {
+                console.log('error while logining a user:', error);
+                res.status(401).send();
+                return;
+            }
+
+            const info = JSON.parse(body);
+
+            res.cookie('token', info.access_token, {
+                expires: new Date(Date.now() + 82800), //23h
+                httpOnly: true,
+            });
+
+            res.status(response.statusCode).send();
+        }
+    );
+};
+
+const register = (req, res) => {
+    request(
+        {
+            method: 'POST',
+            url: `https://${process.env.DOMAIN}/api/v2/users`,
+            headers: {
+                'content-type': 'x-www-form-urlencoded',
+                Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+            },
+            form: {
+                email: req.body.email,
+                given_name: req.body.givenname,
+                family_name: req.body.familyname,
+                name: req.body.name,
+                nickname: req.body.nickname,
+                connection: 'Username-Password-Authentication',
+                password: req.body.password,
+            },
+        },
+        (error, response, body) => {
+            if (error) {
+                console.log('error while creating a user:', error);
+            }
+
+            console.log('Created user status:', response.statusMessage);
+            res.status(response.statusCode).send();
+        }
+    );
+};
+
+const logout = (req, res) => {
+    res.cookie('token', '', {
+        expires: new Date(0),
+        httpOnly: true,
+    });
+
+    res.status(200).send();
+};
+
+const getServerToken = () => {
+    request(
+        {
+            method: 'POST',
+            url: `https://${process.env.DOMAIN}/oauth/token`,
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            form: {
+                client_id: process.env.CLIENT_ID,
+                client_secret: process.env.CLIENT_SECRET,
+                audience: process.env.AUDIENCE,
+                grant_type: 'client_credentials',
+            },
+        },
+        (error, response, body) => {
+            if (error) {
+                console.log('error while getting server token:', error);
+            }
+
+            const info = JSON.parse(body);
+            process.env.ACCESS_TOKEN = info.access_token;
+        }
+    );
+};
+
+module.exports = {
+    login,
+    register,
+    logout,
+    getServerToken,
+};
